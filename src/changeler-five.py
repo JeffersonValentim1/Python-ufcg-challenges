42d0bf6e3ecb73dc3dfe84479f058b2f47b28191
Jefferson Valentim <88254264+JeffersonValentim1@users.noreply.github.com>
2022-05-31 19:40:40 -0300

    06

---
+++ src/changeler-five.py
@@ -0,0 +1,9 @@
+"""
+Função Hello, World!
+
+Escreva a função hello. A função deve ser pura e deve apenas retornar uma string com a mensagem Hello, World!.
+"""
+
+def hello():
+  return 'Hello, World!'
+print(hello())
